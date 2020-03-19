31a259bea26417062bafe93eb22328768108b6c7
Arcot Nishanth <nsharc4@gmail.com>
2020-03-19 17:43:40 +0530

    add css opening screen

---
+++ app.ts
@@ -0,0 +1,15 @@
+(
+  document.onreadystatechange = (): void => {
+    if (document.readyState === 'complete') {
+      const model = {
+
+      };
+      const view = {
+
+      };
+      const controller = {
+
+      };
+    }
+  }
+)();
