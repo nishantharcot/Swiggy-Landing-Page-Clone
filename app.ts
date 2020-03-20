05ecfbc215fde734a9f3519dfdc2d95f517d1a17
Arcot Nishanth <nsharc4@gmail.com>
2020-03-20 10:39:19 +0530

    add basic version

--- app.ts
+++ app.ts
@@ -1,24 +1,23 @@
 (
   document.onreadystatechange = (): void => {
     if (document.readyState === 'complete') {
-      const changingText = document.getElementById('changingText') as HTMLDivElement;
+      const changingText = document.getElementById(
+          'changingText') as HTMLDivElement;
       const model = {
-        displayText: ['Cooking gone wrong?', 'Late Night At Office?', 'Movie Marathon?', 'Unexpected Guests?', 'Game Night?', 'Hungry?'],
-        current: 0
+        displayText: ['Cooking gone wrong?', 'Late Night At Office?',
+          'Movie Marathon?', 'Unexpected Guests?', 'Game Night?', 'Hungry?'],
+        current: 0,
       };
       const view = {
-        init: () => {
+        init: (): void => {
           changingText.innerHTML = model.displayText[model.current];
           model.current = (model.current + 1) % 6;
         },
-        render: () => {
-
-        }
       };
       const controller = {
         init: (): void => {
           setInterval(view.init, 2000);
-        }
+        },
       };
       controller.init();
     }
